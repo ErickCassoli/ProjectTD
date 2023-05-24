518524fc55051d11131d3898932803d6b85aff04
ErickCassoli <erick.mcassoli@gmail.com>
2023-05-24 14:45:15 -0300

    ajuste em formatação e loop no GameScreen

--- RadiantDecay/src/main/GameScreen.java
+++ RadiantDecay/src/main/GameScreen.java
@@ -2,21 +2,41 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
+
 import javax.swing.JPanel;
 
-public class GameScreen extends JPanel{
+public class GameScreen extends JPanel {
 
     private Random random;
-    public GameScreen(){
-        random =  new Random();
+
+    public GameScreen() {
+        random = new Random();
 
     }
-    public void paintConponent(Graphics g){
+
+    public void paintConponent(Graphics g) {
         super.paintComponent(g);
 
-        for(int x = 0; x < 20; x++){
-            g.setColor(new Color(255,10,50));
-            g.fillRect(x * 32, 0, 32, 32);
+        for (int y = 0; y < 20; y++) {
+
+            g.setColor(getRndColor());
+            g.fillRect(y * 32, 0, 32, 32);
+
+            for (int x = 0; x < 20; x++) {
+
+                g.setColor(getRndColor());
+                g.fillRect(x * 32, 0, 32, 32);
+
+            }
         }
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r, g, b);
     }
 }
