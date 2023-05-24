7ab0db08d3386682971c34e955560252d924ca69
ErickCassoli <erick.mcassoli@gmail.com>
2023-05-24 14:23:55 -0300

    for com random color

--- RadiantDecay/src/main/GameScreen.java
+++ RadiantDecay/src/main/GameScreen.java
@@ -6,16 +6,17 @@ import javax.swing.JPanel;
 
 public class GameScreen extends JPanel{
 
+    private Random random;
     public GameScreen(){
+        random =  new Random();
 
     }
     public void paintConponent(Graphics g){
         super.paintComponent(g);
 
-        g.setColor(Color.red);
-
         for(int x = 0; x < 20; x++){
-            
+            g.setColor(new Color(255,10,50));
+            g.fillRect(x * 32, 0, 32, 32);
         }
     }
 }
